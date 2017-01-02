a75e42e0bfe2ea472d307a2e5aa536408196002f
parastelios <parastelios@hotmail.com>
2017-01-02 19:57:35 +0200

    Changes to be committed: 	modified:   server.R

--- server.R
+++ server.R
@@ -20,7 +20,7 @@ library(rJava)
 options(shiny.maxRequestSize = 100*1024^2)
 
 # global settings
-DEBUG_UPLOAD_ON  = T
+DEBUG_UPLOAD_ON  = F
 predictorFile = './data_arcodium/runPre_sub1.csv'
 # predictorFile = './data_arcodium/snowPredictors.csv'
 targetFile = './data_arcodium/runTar_sub1.csv'
